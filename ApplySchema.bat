b33462a27e698bccf75d7768868966b0a41974c6
NNemanjaMM <nemanja.miladinovic@live.com>
2017-08-30 12:16:47 +0200

    app: solution and packages names updated

--- ApplySchema.bat
+++ ApplySchema.bat
@@ -1,9 +1,7 @@
 mkdir temp
 cd temp
-xjc -p com.tas.EDS_Library.diagram %~f1
-echo main-class: AppMainClass>manifest.txt
-echo package com.tas.EDS_Library;import com.tas.EDS_Library.diagram.*;public class AppMainClass{public static void main(String[] args){}}>com\tas\EDS_Library\diagram\AppMainClass.java
-javac com/tas/EDS_Library/diagram/*.java
-jar cfm ../TAS-Solution/lib/model_lib.jar manifest.txt com/tas/EDS_Library/diagram/*.class
+xjc -p com.eds.EDS_Library.diagram %~f1
+javac com/eds/EDS_Library/diagram/*.java
+jar cfvM ../EDS-Solution/lib/model_lib.jar com/eds/EDS_Library/diagram/*.class
 cd ..
 rd /s /q temp
