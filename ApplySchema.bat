350b5c9080d18881185eaa594a68ac5edbf6d57e
NNemanjaMM <nemanja.miladinovic@live.com>
2017-10-23 15:35:20 +0200

    proj: Setup file created, batch file minor update

--- ApplySchema.bat
+++ ApplySchema.bat
@@ -1,10 +1,8 @@
-cd EDS-Solution
-mkdir lib
-cd ..
+
 mkdir temp
 cd temp
-xjc -p com.eds.EDS_Library.diagram %~f1
+xjc -p com.eds.EDS_Library.diagram "%~f1"
 javac com/eds/EDS_Library/diagram/*.java
-jar cfvM ../EDS-Solution/lib/model_lib.jar com/eds/EDS_Library/diagram/*.class
+jar cfvM ../lib/model_lib.jar com/eds/EDS_Library/diagram/*.class
 cd ..
 rd /s /q temp
